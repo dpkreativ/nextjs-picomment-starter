f8c2b702cffffc739f61b38d49959944996e95f0
Divine Orji <dpkreativ@gmail.com>
2022-11-13 19:54:27 +0100

    Update image component in Avatar

--- components/ui/atoms/Avatar.js
+++ components/ui/atoms/Avatar.js
@@ -3,7 +3,11 @@ import Image from 'next/image';
 export default function Avatar() {
   return (
     <div className="avatar">
-      <Image src={`/images/profile.png`} layout="fill" objectFit="contain" />
+      <Image
+        src={`/images/profile.png`}
+        fill
+        style={{ objectFit: 'contain' }}
+      />
     </div>
   );
 }
